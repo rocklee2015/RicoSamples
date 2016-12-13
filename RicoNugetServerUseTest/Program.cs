c3b1411af8728d5fea7e3af39d92ba594bc640ab
rocklee2015 <rocklee0420@163.com>
2016-12-13 14:17:20 +0800

    test nuget refer

---
+++ RicoNugetServerUseTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RicoNugetTest;
+
+namespace RicoNugetServerUseTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 claa1=new Class1();
+            
+        }
+    }
+}
