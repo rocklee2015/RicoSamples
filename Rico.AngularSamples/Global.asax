e57d6daabf7357acf04a943e515a99f5bc5971a1
rocklee2015 <rocklee0420@163.com>
2016-12-16 14:23:31 +0800

    angular,

---
+++ Rico.AngularSamples/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Rico.AngularSamples.MvcApplication" Language="C#" %>
---
+++ Rico.Csharp.Samples/InterfaceSample/IRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rico.Csharp.Samples.InterfaceSample
+{
+    interface IRepository
+    {
+        Task AddAsync();
+    }
+
+    interface IUser: IRepository
+    {
+        [Description("添加用户")]
+        bool AddUser();
+    }
+
+    public class UserRepostory : IUser
+    {
+        public Task AddAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AddUser()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    interface IRepository<T> where T:class 
+    {
+        Task AddAsync(T item);
+    }
+
+
+}
