45f590c182b42e194cb8a5bd2ef3fb9d0233fbc3
rocklee2015 <rocklee0420@163.com>
2016-12-26 12:16:49 +0800

    MVC Samples

---
+++ Mvc/Rico.AspNetMvc.Sample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Rico.AspNetMvc.Sample.MvcApplication" Language="C#" %>
---
+++ Mvc/Rico.AspNetMvc.Sample/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// 有关程序集的常规信息是通过以下项进行控制的
+// 控制。更改这些特性值可修改
+// 与程序集关联的信息。
+[assembly: AssemblyTitle("Rico.AspNetMvc.Sample")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Rico.AspNetMvc.Sample")]
+[assembly: AssemblyCopyright("版权所有(C) Microsoft 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// 将 ComVisible 设置为 false 将使此程序集中的类型
+// 对 COM 组件不可见。如果需要
+// 从 COM 访问此程序集中的某个类型，请针对该类型将 ComVisible 特性设置为 true。
+[assembly: ComVisible(false)]
+
+// 如果此项目向 COM 公开，则下列 GUID 用于 typelib 的 ID
+[assembly: Guid("3784ef0b-b16d-4d1c-869c-84b4390fd27e")]
+
+// 程序集的版本信息由下列四个值组成:
+//
+//      主版本
+//      次版本
+//      内部版本号
+//      修订版本
+//
+// 你可以指定所有值，也可以让修订版本和内部版本号采用默认值，
+// 方法是按如下所示使用 "*":
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
