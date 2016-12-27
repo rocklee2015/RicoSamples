f964e3e139e78b56c56aded6891be2c363a6f673
rocklee2015 <rocklee0420@163.com>
2016-12-27 21:54:46 +0800

    Add Thread Demo

---
+++ Book/Rico.ThreadBookSamples/C5_Csharp5AndAsyncAwait/S9UseawaitToDynamicType.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ImpromptuInterface;
+
+namespace Rico.ThreadBookSamples.C5_Csharp5AndAsyncAwait
+{
+    class S9UseawaitToDynamicType
+    {
+        public static void ExcuteMain()
+        {
+            Task t = AsynchronousProcessing();
+            t.Wait();
+        }
+
+        async static Task AsynchronousProcessing()
+        {
+            string result = await GetDynamicAwaitableObject(true);
+            Console.WriteLine(result);
+
+            result = await GetDynamicAwaitableObject(false);
+            Console.WriteLine(result);
+        }
+
+        static dynamic GetDynamicAwaitableObject(bool completeSynchronously)
+        {
+            dynamic result = new ExpandoObject();
+            dynamic awaiter = new ExpandoObject();
+
+            awaiter.Message = "Completed synchronously";
+            awaiter.IsCompleted = completeSynchronously;
+            awaiter.GetResult = (Func<string>)(() => awaiter.Message);
+
+            awaiter.OnCompleted = (Action<Action>)(callback =>
+              ThreadPool.QueueUserWorkItem(state => {
+                  Thread.Sleep(TimeSpan.FromSeconds(1));
+                  awaiter.Message = GetInfo();
+                  if (callback != null) callback();
+              })
+            );
+
+            IAwaiter<string> proxy = Impromptu.ActLike(awaiter);
+
+            result.GetAwaiter = (Func<dynamic>)(() => proxy);
+
+            return result;
+        }
+
+        static string GetInfo()
+        {
+            return string.Format("Task is running on a thread id {0}. Is thread pool thread: {1}",
+                    Thread.CurrentThread.ManagedThreadId, Thread.CurrentThread.IsThreadPoolThread);
+        }
+    }
+    public interface IAwaiter<T> : INotifyCompletion
+    {
+        bool IsCompleted { get; }
+
+        T GetResult();
+    }
+}
--- Book/Rico.ThreadBookSamples/Program.cs
+++ Book/Rico.ThreadBookSamples/Program.cs
@@ -4,15 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Rico.ThreadBookSamples.C2_ThreadSynchronization;
+using Rico.ThreadBookSamples.C5_Csharp5AndAsyncAwait;
+using Rico.ThreadBookSamples.C9_AsynchronouseIO;
 
 namespace Rico.ThreadBookSamples
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
-            SpinWaitMethod.ExcuteMain(null);
+            //SpinWaitMethod.ExcuteMain(null);
+            //S6AvoidUseSynchrouseContext.ExcuteMain();
+            //S9UseawaitToDynamicType.ExcuteMain();
 
+            //S2AccessFileAsync.ExcuteMain();
+
+            S4AccessDataBaseAsync.ExcuteMain();
             Console.ReadKey();
         }
     }
