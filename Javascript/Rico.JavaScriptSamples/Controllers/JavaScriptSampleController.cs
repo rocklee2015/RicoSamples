f50e071e7a1ba348fe1a88450cf5390e190b4f7e
rocklee2015 <rocklee0420@163.com>
2016-12-30 10:46:50 +0800

    Add Jquery2.0.3 SourceCode Sample

---
+++ Javascript/Rico.JavaScriptSamples/Controllers/JavaScriptSampleController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Rico.JavaScriptSamples.Controllers
+{
+    public class JavaScriptSampleController : Controller
+    {
+        // GET: JavaScriptSample
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>
+        /// JS 属性
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult JsProperty()
+        {
+            return View();
+        }
+        /// <summary>
+        /// JS的原型
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult JsPrototype()
+        {
+            return View();
+        }
+    }
+}
