57c1471e8f4b32c0d9bc8251b6326ec5d492e09a
rocklee2015 <rocklee0420@163.com>
2016-12-30 10:01:07 +0800

    Update T4 Sample

--- Tool/Rico.T4Sample/S1_FromXmlToCs/MessageT4.cs
+++ Tool/Rico.T4Sample/S1_FromXmlToCs/MessageT4.cs
@@ -1,16 +1,17 @@
-﻿                    
-   namespace Rico.T4Sample.S1_FromXmlToCs
-   {
-       public static class Messages
-       {    
-                          public  static class Validation
-               {
-                                  public static MessageEntry MandatoryField = new MessageEntry("MandatoryField","The {0} is mandatory.","Validation");
-                              public static MessageEntry GreaterThan = new MessageEntry("GreaterThan","The {0} must be greater than {1}.","Validation");
-                              }
-                      public  static class Confirmation
-               {
-                                  public static MessageEntry ReallyDelete = new MessageEntry("ReallyDelete","Do you really want to delete the {0}.","Confirmation");
-                              }
+﻿
+namespace Rico.T4Sample.S1_FromXmlToCs
+{
+    public static class Messages
+    {
+	             public static class  Validation
+        {
+			            public static MessageEntry MandatoryField = new MessageEntry("MandatoryField","The {0} is mandatory.","Validation");
+			                 public static MessageEntry GreaterThan = new MessageEntry("GreaterThan","The {0} must be greater than {1}.","Validation");
+			     	    }
+                  public static class  Confirmation
+        {
+			            public static MessageEntry ReallyDelete = new MessageEntry("ReallyDelete","Do you really want to delete the {0}.","Confirmation");
+			     	    }
               }
-   }
+}
+
---
+++ Tool/Rico.T4Sample/readme.md
@@ -0,0 +1,26 @@
+#### 功能介绍
+> - S1: 简单的从XML读取数据
+> - S2:
+> - S3: 生成每个表的的增、删、改、查存储过程
+
+
+#### T4 工具
+>- [T4-Tagnible 模板编辑器 For Visual Studio 2015](http://t4-editor.tangible-engineering.com/Download_T4Editor_Plus_ModelingTools.html)
+
+
+#### 项目来源：
+>- [蒋金楠](http://www.cnblogs.com/artech/archive/2010/10/25/1860064.html)
+
+
+
+#### 遇到问题：
+> - 在Template的InsertProcedureTemplate下关于System.Xml引用问题
+在模板头部加如下代码：
+<#@ assembly name="System.Xml" #>
+<#@ import namespace="System.Xml" #>
+
+> - 要给一个数据库和表，才能生成存储过程
+  再有就是Server server=new Server();需要一个数据库实例；
+
+> - 继承Template模板，需要引用T4Toolbox程序集
+<#@ assembly name="C:\Users\Administrator\AppData\Local\Microsoft\VisualStudio\12.0\Extensions\ieh5mgg0.kxr\T4Toolbox.dll" #>
