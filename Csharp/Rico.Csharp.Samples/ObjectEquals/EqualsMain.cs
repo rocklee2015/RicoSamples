ceb6aec51a6e4f53056860b5c461b4fc9dea3265
rocklee <rocklee@DESKTOP-RMAQMNE>
2017-01-03 23:13:18 +0800

    Add PlinqQuery And Reactive Extension

---
+++ Csharp/Rico.Csharp.Samples/ObjectEquals/EqualsMain.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rico.Csharp.Samples.ObjectEquals
+{
+    class EqualsMain
+    {
+        public void ExcuteMain()
+        {
+            var obj1=new EqualsMain();
+            var obj2 = obj1;
+            var result=Equals(obj1, obj2);
+        }
+    }
+}
