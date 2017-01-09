24bba2557ebee821fb83e7a7f2863f2adf22f97d
rocklee2015 <rocklee0420@163.com>
2017-01-09 10:59:21 +0800

    CNblog CSS ricolee

---
+++ BootstrapUI/CnblogsCss/RicoLee/cnblogs-customfunc.js
@@ -0,0 +1,197 @@
+﻿/*
+功能：	为博客自定义功能，比如推荐此文，Scrollbar;roadmap:新功能添加在此
+参考：	http://www.cnblogs.com/marvin/p/ExtendWizNoteAutoNnavigation.html
+		http://www.cnblogs.com/asxinyu/p/Bolg_Category_For_BlogBeauty.html
+*/
+
+jQuery.fn.wait = function(func, times, interval) {
+    var _times = times || -1,
+        //100次
+        _interval = interval || 20,
+        //20毫秒每次 
+        _self = this,
+        _selector = this.selector,
+        //选择器
+        _iIntervalID; //定时器id
+    if (this.length) { //如果已经获取到了，就直接执行函数
+        func && func.call(this);
+    } else {
+        _iIntervalID = setInterval(function query() {
+            if (!_times) { //是0就退出
+                clearInterval(_iIntervalID);
+            }
+            _times <= 0 || _times--; //如果是正数就 --
+            _self = $(_selector); //再次选择
+            if (_self.length) { //判断是否取到
+                func && func.call(_self);
+                clearInterval(_iIntervalID);
+            }
+        }, _interval);
+    }
+    return this;
+}
+
+function focusFunction() {
+    var _targetTop = $('#comment_form_container').offset().top; //获取位置
+    jQuery("html,body").animate({
+        scrollTop: _targetTop
+    }, 300); //跳转
+}
+
+function focusFollow() {
+    var _targetTop = $('#profile_block').offset().top; //获取位置
+    jQuery("html,body").animate({
+        scrollTop: _targetTop
+    }, 300); //跳转
+}
+
+//TODO 设置子级div的宽度，最佳解决方案：让它撑满父DIV
+var calcPencent = '110%';
+function getContentWidth(compareWidth){
+    //在1920的分辨率中，foxmail订阅默认大小<1440
+    var screePix = new Array(800,1024,1280,1440,1600,1920,2560);
+    var screePencent = new Array(1.28,1.26,1.24,1.22,1.2,1.18,1.1);
+    var isGetVal = false;
+    $.each(screePix, function(idx, obj) {
+        //console.log(idx,parseInt(obj),parseInt(compareWidth));
+        if(parseInt(obj) >= parseInt(compareWidth) && !isGetVal){
+            var calcWidth = parseInt(screePencent[idx] * compareWidth);
+            var tmpPencent = (screePencent[idx] * 100);
+            calcPencent = (tmpPencent.toString()+"%");
+            console.log('arrary:',idx,obj,compareWidth,calcPencent,calcWidth);
+            isGetVal = true;
+            return calcPencent;
+        }
+    });
+}
+
+function consoleInfo(){
+
+}
+
+$(document).ready(function() {
+    $(window).resize(function(){
+        /*if (document.getElementById("cnblogs_post_body")){
+            var maxWidth = $(document.body).width();
+            console.log("window.resize:"+$('#main').width()+",bodyWidth:"+maxWidth);
+            $('#mainContent').css('width',maxWidth);
+        }*/
+    });
+var md = new MobileDetect(window.navigator.userAgent);
+ if (md.mobile()){}else {
+        consoleInfo();
+        $("<div id='toTop'  title='回顶部'></div>").appendTo($("body"));
+        $("#toTop").bind("click", function() {
+            $("body,html").animate({
+                scrollTop: '0px'
+            }, 200);
+        });
+        $("<div id='toBottom' title='到底部'></div>").appendTo($("body"));
+        $("#toBottom").bind("click", function() {
+            //文章页下拉到评论区，其它页面拉到顶部
+            if (document.getElementById("blog_post_info_block")) {
+                $("body,html").animate({
+                    scrollTop: $('#blog_post_info_block').offset().top
+                }, 150);
+                console.log("page is article");
+            } else {
+                $("body,html").animate({
+                    scrollTop: $('#footer').offset().top
+                }, 150);
+                console.log("page not article");
+            }
+        });
+    }
+    //版权信息
+    var signatureHtml ="";
+    signatureHtml +=  '作者：ricolee &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 出处：<a href="http://www.cnblogs.com/kindle/">http://www.cnblogs.com/kindle/</a><br />';
+    signatureHtml += '本文版权归作者和博客园共有欢迎转载，转载之后请务必在文章明显位置标出原文链接和作者，谢谢。<br />';
+    signatureHtml += '如果本文对您有帮助，请点击<a id="recommendme",href="javascript:void(0);">【推荐】</a>您的赞赏将鼓励我继续创作！想跟我一起进步么？那就<a id="followme" href="javascript:void(0);">【关注】</a>我吧。';
+    signatureHtml += '<div id="signatureTips"></div>';
+    if ($("#cnblogs_post_body").next().attr("id") == "MySignature") {
+        $("#cnblogs_post_body #MySignature").hide();
+        $("#cnblogs_post_body").next().show().html(signatureHtml);
+    }
+    else {
+        $("#cnblogs_post_body #MySignature").show().html(signatureHtml);
+    }
+    //fix：把click函数写在Html代码里在渲染之后莫名多了个'='
+    $('#recommendme').on('click', function() {
+        votePost(cb_entryId,"Digg")
+        console.log("recommend success");
+        $("#signatureTips").html("感谢您的推荐！");
+    });
+    $('#followme').on('click', function() {
+        cnblogs.UserManager.FollowBlogger("a7e65336-5c2c-e111-b988-842b2b196315");
+        console.log("follow success");
+        $("#signatureTips").html("谢谢您的关注！");
+    });  
+});
+
+
+/*浮动工具条*/
+$("#div_digg").wait(function() {
+    var html = '<div style="padding-bottom: 5px;">';
+    if ($("#p_b_follow") != null) {
+        if ($("#p_b_follow").text().indexOf("加关注") != -1) {
+            html += '<a onclick="javascript:focusFollow();" href="javascript:void(0);">关注我</a>&nbsp;|&nbsp;';
+        }
+    }
+    html += '<a onclick="javascript:focusFunction();" href="javascript:void(0);">快速评论</a>';
+    html += '</div>';
+
+    $(html).appendTo('#div_digg');
+    //console.log('wait digg');
+});
+
+$("#mainContent").wait(function() {
+    //根据页面type
+   if (document.getElementById("cnblogs_post_body")){
+        //博客内容全屏  NOTE 在IE下不用设置width，隐藏同级div，它就是全屏 v~v，buf chrome并不是!
+        $('#sideBar').css('width','0px');
+        $('#sideBar').css('display','none');
+
+        var maxWidth = $(document.body).width();
+        console.log("#main.Width:"+$('#main').width()+",bodyWidth:"+maxWidth);
+        var contentWidth = $('#main').width() - 20;
+        getContentWidth(maxWidth);    
+        $('#mainContent').css('width',calcPencent);
+        //$('#mainContent').css('position','relative');
+        console.log('this page fullscreen->', calcPencent.toString(),$('#mainContent').width()+"px");
+   }else{
+        //常显示右侧div
+        $('#sideBar').css('display','block');
+        $('#mainContent').css('width','100%');
+        console.log('this page normalsize');
+   }
+});
+   
+/** 
+ * js截取字符串，中英文都能用 
+ * 如果给定的字符串大于指定长度，截取指定长度返回，否者返回源字符串。  
+ * @param str：需要截取的字符串 
+ * @param len: 需要截取的长度 
+ */
+function cutstr(str, len) {
+    var str_length = 0;
+    var str_len = 0;
+    str_cut = new String();
+    str_len = str.length;
+    for (var i = 0; i < str_len; i++) {
+        a = str.charAt(i);
+        str_length++;
+        if (escape(a).length > 4) {
+            //中文字符的长度经编码之后大于4  
+            str_length++;
+        }
+        str_cut = str_cut.concat(a);
+        if (str_length >= len) {
+            str_cut = str_cut.concat("...");
+            return str_cut;
+        }
+    }
+    //如果给定字符串小于指定长度，则返回源字符串；  
+    if (str_length < len) {
+        return str;
+    }
+}
---
+++ BootstrapUI/CnblogsCss/ZhaoQingQing/cnblogs-article-nav.js
@@ -0,0 +1,227 @@
+﻿/*
+功能：	自动为博客园文章生成目录
+原理：	抓取页面中的h1,h2,h3，生成<li>
+参考：	http://www.cnblogs.com/marvin/p/ExtendWizNoteAutoNnavigation.html
+	  http://www.cnblogs.com/asxinyu/p/Bolg_Category_For_BlogBeauty.html
+*/
+
+//cnblogs生成右侧目录 start
+$(document).ready(function() {
+    var b = $('body'),
+        cnblogs_post_body = 'cnblogs_post_body',
+        sideNavBody = 'sideToolbar',
+        sideCatalog = 'sideCatalog',
+        f = 'sideCatalog-catalog',
+        sideCatalogCtrl = 'sideCatalogBtn',
+        h = 'sideToolbar-up',
+        //默认显示文章目录
+        navcontaint = '<div id="sideToolbar">\<div class="sideCatalogBg"id="sideCatalog">\<div id="sideCatalog-sidebar">\<div class="sideCatalog-sidebar-top"></div>\<div class="sideCatalog-sidebar-bottom"></div>\</div>\<div id="sideCatalog-catalog">\<ul class="nav"style="width:300px;zoom:1">\</ul>\</div>\</div>\</div>\<a href="javascript:void(0);" title="[隐藏/显示]目录" id="sideCatalogBtn" class="sideCatalogBtnDisable"></a>',
+        j = '',
+        k = 200,
+        l = 0,
+        m = 0,
+        n = 0,
+        o, p = 18,
+        q = true,
+        r = true,
+        cnblogs_post_body_flag = $('#' + cnblogs_post_body);
+   
+    //mobile detect
+    var md = new MobileDetect(window.navigator.userAgent);
+    if (md.mobile()) {
+        
+        //$('#mainContent').css('style','1');
+         console.log("you platform is mobile");
+         return;
+    }else{
+        console.log("you platform not mobile ");
+    }
+
+    //只有文章页面才会生成    
+    if (cnblogs_post_body_flag.length === 0) {
+        return
+    };
+    b.append(navcontaint);
+    o = cnblogs_post_body_flag.find(':header');
+    if (o.length > p) {
+        r = false;
+        var t = cnblogs_post_body_flag.find('h2');
+        var u = cnblogs_post_body_flag.find('h3');
+        if (t.length + u.length > p) {
+            //q = false //如果h2和h3太多，依然要生成h3
+        }
+    };
+
+    o.each(function(t) {
+        var u = $(this),
+            v = u[0];
+
+        var title = u.text();
+        var text = u.text();
+
+        u.attr('id', 'autoid-' + l + '-' + m + '-' + n)
+            //if (!u.attr('id')) {
+            //    u.attr('id', 'autoid-' + l + '-' + m + '-' + n)
+            //};
+
+        /*文章目录节点：
+		    标题1/标题2: <li>
+			    标题3: h2Offset
+                    标题4: h3Offset
+		*/
+        if (v.localName === 'h1' || v.localName === 'h2') {
+            l++;
+            m = 0;
+            //超过30字，后面加....
+            if (text.length > 30) text = text.substr(0, 30) + "...";
+            //h1,h2,h3 如果需要在前面自动生成序号就添加<li><span>' + l + '.&nbsp</span>)
+            //j += '<li><span>' + l + '.&nbsp</span><a href="#' + u.attr('id') + '" title="' + title + '">' + text + '</a><span class="sideCatalog-dot"></span></li>';
+            j += '<li><a href="#' + u.attr('id') + '" title="' + title + '">' + text + '</a><span class="sideCatalog-dot"></span></li>';
+        } else if (v.localName === 'h3') {
+            m++;
+            n = 0;
+            if (q) {
+                if (text.length > 28) text = text.substr(0, 28) + "...";
+                j += '<li class="h2Offset"><a href="#' + u.attr('id') + '" title="' + title + '">' + text + '</a></li>';
+            }
+        } else if (v.localName === 'h4') {
+            n++;
+            if (r) {
+                if (text.length > 26) text = text.substr(0, 26) + "...";
+                j += '<li class="h3Offset"><a href="#' + u.attr('id') + '" title="' + title + '">' + text + '</a></li>';
+            }
+        }
+    });
+    $('#' + f + '>ul').html(j);
+    b.data('spy', 'scroll');
+    b.data('target', '.sideCatalogBg');
+    $('body').scrollspy({
+        target: '.sideCatalogBg'
+    });
+    $sideCatelog = $('#' + sideCatalog);
+    $('#' + sideCatalogCtrl).on('click', function() {
+        if ($(this).hasClass('sideCatalogBtnDisable')) {
+            $("#"+sideNavBody).css('display', 'none')
+            $('#' + sideCatalogCtrl).title = "隐藏目录";
+        } else {
+            $("#"+sideNavBody).css('display', 'block')
+            $('#' + sideCatalogCtrl).title = "显示目录";
+        };
+        $(this).toggleClass('sideCatalogBtnDisable')
+    });
+    $('#' + h).on('click', function() {
+        $("html,body").animate({
+            scrollTop: 0
+        }, 500)
+    });
+    $sideToolbar = $('#' + sideNavBody);
+    $(document).on('scroll', function() {
+        var t = $(document).scrollTop();
+        //我要一直显示文章目录
+         if (t > k) {
+            $sideToolbar.css('display', 'block')
+        } else {
+            $sideToolbar.css('display', 'none')
+        } 
+    })
+});
+//cnblogs生成右侧目录 end
+
+// 在文章正文顶部生成TOC start
+$(document).ready(function() {
+    buildTocTable();
+    });
+
+function buildTocTable() {
+    var hs = $('#cnblogs_post_body').find('h1,h2,h3,h4,h5,h6');
+    if (hs.length < 2)
+        return;
+    var s = '';
+    s += '<div style="clear:both"></div>';
+    s += '<div class="cnblogs_toc">';
+    s += '<p style="text-align:left;margin:0;"><span style="float:left; text-indent:0;"><a id="TocTitle" href="#" onclick="javascript:return OnTitleShowToc(this);" title="系统根据文章中H1到H6标签自动生成文章目录">文章目录[点击展开](?)</a></span><a id="TocTitleSymbol" href="#" onclick="javascript:return OnTitleSymbolShowToc(this);" title="展开">[+]</a></p>';
+    //.cnblogs_toc ol 控制默认展开或关闭
+    s += '<ol style="display:none;margin-left:14px;padding-left:14px;line-height:160%;">';
+    var old_h = 0, ol_cnt = 0;
+    for (var i = 0; i < hs.length; i++) {
+        var h = parseInt(hs[i].tagName.substr(1), 10);
+        if (!old_h)
+            old_h = h;
+        if (h > old_h) {
+            s += '<ol>';
+            ol_cnt++;
+        }
+        else if (h < old_h && ol_cnt > 0) {
+            s += '</ol>';
+            ol_cnt--;
+        }
+        if (h == 1) {
+            while (ol_cnt > 0) {
+                s += '</ol>';
+                ol_cnt--;
+            }
+        }
+        old_h = h;
+        var tit = hs.eq(i).text().replace(/^\d+[.、\s]+/g, '');
+        tit = tit.replace(/[^a-zA-Z0-9_\-\s\u4e00-\u9fa5]+/g, '');
+
+        if (tit.length < 100) {
+            s += '<li><a href="#t' + i + '">' + tit + '</a></li>';
+            hs.eq(i).html('<a name="t' + i + '"></a>' + hs.eq(i).html());
+        }
+    }
+    while (ol_cnt > 0) {
+        s += '</ol>';
+        ol_cnt--;
+    }
+    s += '</ol></div>';
+    s += '<div style="clear:both"><br></div>';
+    $(s).insertBefore($('#cnblogs_post_body'));
+    //note 若toc过长，影响阅读体验，默认收起
+    //mobile detect
+    // var md = new MobileDetect(window.navigator.userAgent);
+    // if (md.mobile()) {
+    //     $('.cnblogs_toc ol').css('display','none');
+    // }else{
+    //    $('.cnblogs_toc ol').css('display','block');
+    // }
+}
+
+var isOpendToc = false;
+
+function UpdateTocTitle(){
+    if(isOpendToc){
+        $("#TocTitleSymbol").attr('title', '收起').html('[-]');
+        $("#TocTitle").html('文章目录[点击收起](?)');
+    }else{
+        $("#TocTitleSymbol").attr('title', '展开').html('[+]');
+        $("#TocTitle").html('文章目录[点击展开](?)');
+    }
+}
+
+function OnTitleShowToc(e) {
+    if (!isOpendToc) {
+        $(e).parent().parent().next().show();
+        isOpendToc = true;
+    } else {
+        $(e).parent().parent().next().hide();
+        isOpendToc = false;
+    }
+    UpdateTocTitle();
+    e.blur();
+    return false;
+}
+
+function OnTitleSymbolShowToc(e) {
+    if (!isOpendToc) {
+        $("#TocTitleSymbol").parent().next().show();
+        isOpendToc = true;
+    } else {
+        $("#TocTitleSymbol").parent().next().hide();
+        isOpendToc = false;
+    }
+    UpdateTocTitle();
+    e.blur();
+    return false;
+}
+// 在文章正文顶部生成TOC end
