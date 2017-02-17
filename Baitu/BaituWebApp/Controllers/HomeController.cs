26a12a706507716883d5f270503eb15d47e01e07
rocklee2015 <rocklee0420@163.com>
2017-02-17 16:48:28 +0800

    baitu test

--- Baitu/BaituWebApp/Controllers/HomeController.cs
+++ Baitu/BaituWebApp/Controllers/HomeController.cs
@@ -3,24 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Rico.EF6Samples;
 
-namespace Rico.MiniProfilterSample.Controllers
+namespace BaituWebApp.Controllers
 {
     public class HomeController : Controller
     {
         // GET: Home
         public ActionResult Index()
         {
-            using (RicoDbContext dbContext = new RicoDbContext())
-            {
-                ViewBag.data = dbContext.Students.ToList();
-            }
-
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult FitVids()
         {
             return View();
         }
