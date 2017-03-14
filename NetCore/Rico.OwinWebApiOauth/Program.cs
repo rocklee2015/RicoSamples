eeddece468e94feaba23c9b1e0011e5edc7b86cd
李 宽 <rocklee0420@163.com>
2017-03-14 12:56:45 +0800

    Owin Oauth

---
+++ NetCore/Rico.OwinWebApiOauth/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
+
+namespace Rico.OwinWebApiOauth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WebApp.Start<Startup>("http://localhost:9000"))
+            {
+                Console.WriteLine("Press [enter] to quit...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
