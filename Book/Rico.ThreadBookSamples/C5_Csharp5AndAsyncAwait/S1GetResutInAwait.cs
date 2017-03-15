77831c4f3e8ea4d62ae4367ac80e7f646c288777
李 宽 <rocklee0420@163.com>
2017-03-15 10:53:02 +0800

    update await example

--- Book/Rico.ThreadBookSamples/C5_Csharp5AndAsyncAwait/S1GetResutInAwait.cs
+++ Book/Rico.ThreadBookSamples/C5_Csharp5AndAsyncAwait/S1GetResutInAwait.cs
@@ -8,28 +8,61 @@ using System.Threading.Tasks;
 
 namespace Rico.ThreadBookSamples.C5_Csharp5AndAsyncAwait
 {
+    /// <summary>
+    /// C# 5.0 await 使用案例
+    /// </summary>
     class S1GetResutInAwait
     {
         public static void ExcuteMain()
         {
-            //Task t = AsynchronyWithTPL();
-            //t.Wait();
             Console.WriteLine("(ExcuteMain)Current Thread Id {0},Is Thread Pool Thread:{1}", Thread.CurrentThread.ManagedThreadId, Thread.CurrentThread.IsThreadPoolThread);
-            AsynchronyWithAwait();
-            //Task t = AsynchronyWithAwait();
-            //t.Wait();
+
+            //使用TPL的方式
+            Task taskWithTpl = AsynchronyWithTpl();
+            taskWithTpl.Wait();
+            
+            //直接调用
+            //AsynchronyWithAwait();
+
+            //使用awati
+            Task taskWithAwait = AsynchronyWithAwait();
+            taskWithAwait.Wait();
+            
+
+            //将异步的结果放在 task 中，taskNoReturn 是没有Result属性的，有返回值才有
+            //Task taskNoReturn = AsynchronyWithAwaitNoReturn();
+            //taskNoReturn.Wait();//阻塞当前主线程
+
+
+            //直接执行Task 函数，是异步执行，调用完成之前主线程继续执行，
+            //GetInfoAsyncNoReturn("TaskNoReturn1", 1);
+            //GetInfoAsyncNoReturn("TaskNoReturn2", 1);
+            //GetInfoAsyncNoReturn("TaskNoReturn3", 1);
+            //GetInfoAsyncNoReturn("TaskNoReturn4", 1);
+
+            Console.WriteLine("主线程------");
             Console.ReadKey();
         }
 
-        static Task AsynchronyWithTPL()
+        static Task AsynchronyWithTpl()
         {
-            Task<string> t = GetInfoAsync("Task 1",2);
+            Task<string> t = GetInfoAsync("Task 1", 2);
             Task t2 = t.ContinueWith(task => Console.WriteLine(t.Result),
                 TaskContinuationOptions.NotOnFaulted);
             Task t3 = t.ContinueWith(task => Console.WriteLine(t.Exception.InnerException),
                 TaskContinuationOptions.OnlyOnFaulted);
 
             return Task.WhenAny(t2, t3);
+        }
+
+        static Task AsynchronyWithAwaitNoReturn()
+        {
+            Console.WriteLine("(AsynchronyWithAwaitNoReturn)Current Thread Id {0},Is Thread Pool Thread:{1}", Thread.CurrentThread.ManagedThreadId, Thread.CurrentThread.IsThreadPoolThread);
+
+            GetInfoAsyncNoReturn("TaskNoReturn1", 1);
+            GetInfoAsyncNoReturn("TaskNoReturn2", 1);
+            var result = GetInfoAsyncNoReturn("TaskNoReturn", 1);
+            return result;
         }
 
         static async Task AsynchronyWithAwait()
@@ -53,7 +86,7 @@ namespace Rico.ThreadBookSamples.C5_Csharp5AndAsyncAwait
                 sw.Start();
                 // result3 没有await,但是异步函数确实执行了，只是没有获取执行结果。这时result3只是Task,等用await,或者.Result（还有其它方式）取结果时，才切换至主线程
                 var result3 = GetInfoAsync("Task 3", 1);
-                var result4 =  await GetInfoAsync("Task 4", 1);
+                var result4 = await GetInfoAsync("Task 4", 1);
                 Console.WriteLine(result4);
                 Console.WriteLine(result3.Result);
                 //Console.WriteLine(await result3); 同上一行代码
@@ -78,13 +111,19 @@ namespace Rico.ThreadBookSamples.C5_Csharp5AndAsyncAwait
                 Console.WriteLine(ex);
             }
         }
-        static async Task<string> GetInfoAsync(string name,int delaySecond)
+        static async Task<string> GetInfoAsync(string name, int delaySecond)
         {
             await Task.Delay(TimeSpan.FromSeconds(delaySecond));
-            Console.WriteLine("{0} is running ",name);
+            Console.WriteLine("{0} is running ", name);
             //throw new Exception("Boom!");
-            return string.Format("Task {0} is running on a thread id {1}. Is thread pool thread: {2}",
-                name, Thread.CurrentThread.ManagedThreadId, Thread.CurrentThread.IsThreadPoolThread);
+            return $"Task {name} is running on a thread id {Thread.CurrentThread.ManagedThreadId}. Is thread pool thread: {Thread.CurrentThread.IsThreadPoolThread}";
+        }
+
+        static async Task GetInfoAsyncNoReturn(string name, int delaySecond)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(delaySecond));
+            Console.WriteLine("{0} is running ", name);
+            Console.WriteLine($"Task {name} is running on a thread id {Thread.CurrentThread.ManagedThreadId}. Is thread pool thread: {Thread.CurrentThread.IsThreadPoolThread}");
         }
     }
 }
