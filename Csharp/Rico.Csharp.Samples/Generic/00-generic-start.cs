a7de79d4b879db4733886ba6d395a4678345ff27
李 宽 <rocklee0420@163.com>
2017-03-23 16:19:53 +0800

    泛型的协变和逆变

---
+++ Csharp/Rico.Csharp.Samples/Generic/00-generic-start.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rico.Csharp.Samples.Generic
+{
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    public class _00_generic_start
+    {
+    }
+}
