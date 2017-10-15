b72582682adc9ae26d07613c057dbbb977d95cad
rocklee2015 <rocklee0420@163.com>
2017-10-15 13:19:04 +0800

    net core sample

---
+++ 03-netcore/net core command/app/applib/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace applib
+{
+    public class Class1
+    {
+        public void PrintOut()
+        {
+            System.Console.WriteLine("class1 in app lib");
+        }
+        public string GetStr(){
+            return "return from lib";
+        }
+    }
+}
