9002d1e8fac5e0e671f5266e38fe78d731aabe13
rocklee2015 <rocklee0420@163.com>
2017-11-10 18:10:21 +0800

    vue and mvc

---
+++ 04-javascript/Rico.Vuejs/FirstMvcAndVue/FirstMvcAndVue/Controllers/VueDemoController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstMvcAndVue.Controllers
+{
+    public class VueDemoController : Controller
+    {
+        // GET: VueDemo
+        public ActionResult First()
+        {
+            return View();
+        }
+        public ActionResult FormDemo()
+        {
+            return View();
+        }
+        public ActionResult VueComponent()
+        {
+            return View();
+        }
+    }
+}
