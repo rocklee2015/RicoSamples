7cc2b4e4ac4435630454e696e5e218062ec4428c
rocklee2015 <rocklee0420@163.com>
2018-01-03 11:12:58 +0800

    qiniu image change path

---
+++ 01-csharp/Rico.S00.Csharp5/ConditionCompileTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rico.S00.Csharp5
+{
+    [TestClass]
+    public class ConditionCompileTest
+    {
+        [TestMethod]
+        public void Condition_If_ElseIf() {
+            var condition = 1;
+#if DEBUG
+            condition=2;
+#else 
+            condition=3;
+#endif
+            Console.WriteLine(condition);
+        }
+    }
+}
---
+++ 01-csharp/Rico.S00.Csharp6/ConditionCompileTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rico.S00.Csharp6
+{
+    [TestClass]
+    public class ConditionCompileTest
+    {
+        [TestMethod]
+        public void Condition_If_ElseIf()
+        {
+            var condition = 1;
+#if DEBUG
+            condition = 2;
+#else
+            condition=3;
+#endif
+
+#if DEBUG
+            Assert.IsTrue(condition == 2);
+#else 
+             Assert.IsTrue(condition == 3);
+#endif
+        }
+    }
+}
