e48bb7f73fe281f0d50e1349453421ec02e9d40a
rocklee2015 <rocklee0420@163.com>
2018-02-24 17:12:19 +0800

    ms unit test

--- 08-test/Rico.MoqSample/ICustomer.cs
+++ 08-test/Rico.MoqSample/ICustomer.cs
@@ -6,5 +6,22 @@ using System.Threading.Tasks;
 
 namespace Rico.MoqSample
 {
-  
+    public interface ICustomer
+    {
+
+        void AddCall();
+
+        string GetCall();
+
+        string GetCall(string strUser);
+
+        string GetAddress(string strUser, out string Address);
+
+        string GetFamilyCall(ref string strUser);
+
+        void ShowException(string str);
+
+
+
+    }
 }
