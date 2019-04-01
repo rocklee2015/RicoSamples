9467f9b7496227bb3a1375a46eb1343d8db5689b
rocklee2015 <rocklee0420@163.com>
2019-04-01 16:22:44 +0800

    you must know

---
+++ 09-book-sample/Rico.S02.YouMustKnowNet/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rico.S02.YouMustKnowNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s01_string = new S01_String();
+
+            s01_string.Execute1_1();
+            s01_string.Execute1_2();
+            s01_string.Execute1_3();
+
+            s01_string.Execute2_1();
+            s01_string.Execute2_2();
+            s01_string.Execute2_3();
+
+            s01_string.Execute3_1();
+            s01_string.Execute3_2();
+        }
+    }
+}
---
+++ 09-book-sample/Rico.S02.YouMustKnowNet/S01_String.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rico.S02.YouMustKnowNet
+{
+    public class S01_String
+    {
+        public void Execute1_1()
+        {
+            string s1 = "abc";
+            Console.WriteLine(string.IsInterned(s1) ?? "null");
+        }
+
+        public void Execute1_2()
+        {
+            string s1 = "ab";
+            s1 += "c";
+            Console.WriteLine(string.IsInterned(s1) ?? "null");
+
+        }
+
+        public void Execute1_3()
+        {
+            string s1 = "abc";
+            string s2 = "ab";
+            s2 += "c";
+
+            string s3 = "ab";
+
+            Console.WriteLine(string.IsInterned(s1) ?? "null");
+            Console.WriteLine(string.IsInterned(s2) ?? "null");
+
+            Console.WriteLine(string.IsInterned(s3) ?? "null");
+
+        }
+        //---------------第二关-------------------------
+        public void Execute2_1()
+        {
+            string s1 = "abc";
+            string s2 = "ab";
+            string s3 = s2 + "c";
+
+            Console.WriteLine(string.IsInterned(s3) ?? "null");
+
+        }
+
+        public void Execute2_2()
+        {
+            string s2 = "ab";
+            s2 += "c";
+
+            Console.WriteLine(string.IsInterned(s2) ?? "null");
+
+            string s1 = "abc";
+
+        }
+
+
+        public void Execute2_3()
+        {
+            string s2 = "ab";
+            s2 += "c";
+
+            Console.WriteLine(string.IsInterned(s2) ?? "null");
+
+            string s1 = GetStr();
+
+        }
+        private static string GetStr()
+        {
+            return "abc";
+        }
+        //---------------第三关-------------------------
+
+        public const string s7 = "abc";
+        public static string s8 = "abc";
+        public void Execute3_1()
+        {
+            string s2 = "ab";
+            s2 += "c";
+            Console.WriteLine(string.IsInterned(s2) ?? "null");
+        }
+        public void Execute3_2()
+        {
+            string s2 = "ab";
+            s2 += "c";
+            Console.WriteLine(string.IsInterned(s2) ?? "null");
+        }
+    }
+}
