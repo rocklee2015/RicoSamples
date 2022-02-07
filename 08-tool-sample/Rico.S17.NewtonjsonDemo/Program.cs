4b52a80cfbfc595056d75b36c377e80de3195ef4
ricolee <rocklee0420@163.com>
2022-02-07 17:07:21 +0800

    mvc

---
+++ 08-tool-sample/Rico.S17.NewtonjsonDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace Rico.S17.NewtonjsonDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> Content = new Dictionary<string, string>();
+            Content.Add("bulletinContent", "this is bulletinContent");
+            Content.Add("topToDay", "this is topToDay");
+
+            //Dictionary转json
+            string Contentjson = JsonConvert.SerializeObject(Content);
+            Console.WriteLine(Contentjson);
+            //json转Dictionary
+            Dictionary<string, string> DicContent = (Dictionary<string, string>) JsonConvert.DeserializeObject(Contentjson.ToString());
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 14-Mvc/ASPNET-MVC/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using ASPNET_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using X.PagedList;
+
+namespace ASPNET_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public List<User> Users;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            Users = Enumerable.Range(1, 1000).Select(user => new User()
+            {
+                Id = user,
+                Name = "张三" + user,
+                Age = user + 10
+            }).ToList();
+        }
+
+        public IActionResult Index(int page = 1)
+        {
+            var result=Users.ToPagedList(page, 10);
+            return View(result);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
